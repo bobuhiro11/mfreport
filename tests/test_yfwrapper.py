b21ce4ebd8cbb722cf54f35b591094efd30c5558
Nobuhiro MIKI <nob@bobuhiro11.net>
2022-08-31 09:36:05 +0900

    fix short name
    Signed-off-by: Nobuhiro MIKI <nob@bobuhiro11.net>

--- tests/test_yfwrapper.py
+++ tests/test_yfwrapper.py
@@ -10,7 +10,7 @@ class TestYfwrapper(unittest.TestCase):
         units = pd.Series(data=[1, 1], index=["9984.T", "V"])
         df = Yfwrapper(units).get_info()
 
-        self.assertEqual("Visa Inc.", df.at["V", "shortName"])
+        self.assertEqual("Visa", df.at["V", "shortName"])
         self.assertGreater(df.at["V", "divTotal"], 100.0)
         self.assertGreater(df.at["V", "divAug"], 10.0)
         self.assertEqual(df.at["V", "divDec"], 0.0)
